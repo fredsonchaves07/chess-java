734045ec6e5b2098b3a636c70bfc1bc3c1923724
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-26 12:42:23 -0300

    feat: printMatch implementation

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,11 +9,22 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
     private final Board board;
-    private Piece piece;
+    private int turn;
+    private Color currentPlayer;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -38,11 +49,13 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) throw new ChessException("There is not piece on source position");
+        if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) throw new ChessException("The chosen piece is not yours");
         if (!board.piece(position).isThereAnyPossibleMove()) throw new ChessException("There is not possible moves");
     }
 
@@ -55,6 +68,11 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(removedPiece, target);
         return capturedPiece;
+    }
+
+    private void nextTurn() {
+        turn ++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
